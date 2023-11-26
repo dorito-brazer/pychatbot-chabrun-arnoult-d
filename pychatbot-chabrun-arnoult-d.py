ed6cbb49a8abeccee193334832aae5369fbe265b
Quentin <151142649+dorito-brazer@users.noreply.github.com>
2023-11-26 17:10:34 +0100

    Update pychatbot-chabrun-arnoult-d.py

--- pychatbot-chabrun-arnoult-d.py
+++ pychatbot-chabrun-arnoult-d.py
@@ -113,26 +113,6 @@ def score_tf_idf(files_names):
     for el in dico_tf:
         dico_tf_idf[el] = dico_tf[el][0]*dico_idf[el]
     return dico_tf_idf     
-
-def tf_idf_nul(dico): # Lorsque l element est egal a 0 alors la cle est ajoute a une liste "nul"
-    nul = []
-    for el in dico:
-        if dico[el] == 0 :
-            nul.append(el)
-    return nul
-
-def matrice_res(files_names):
-    contenu_fich=[]
-    ind=0
-    for i in range(len(files_names)):
-        with open("cleaned/"+f_names, "r", encoding="utf8") as fich:
-            élément=fich.read
-            contenu_fich=.append(contenu)
-        ind+=1
-    matr_res=[]
-    for fich in range(len(contenu_fich)):
-       ligne=[]
-        for i in range 
     
 def matrice(files_names):
     matrice = {}
@@ -145,15 +125,6 @@ def matrice(files_names):
             matrice[el] = dico_tf_idf[el]
         idx+= 1
     return matrice
-                
-def verif_matrice(matrice):
-    idx = 0
-    lst = []
-    for el in matrice:
-        if len(matrice[el]) != 8:
-            lst.append(el)
-            idx +=1
-    return idx,lst
 
 def mots_pas_imp(matrice): # Parcours chaque mot dans la matrice contenant les discours et si leur score tf-idf=0, le met danns la liste mots_pas_imp
     mots_pas_imp = []
