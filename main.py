f25f0ebeff5d3a12e6020888ca5c918cf8f99398
Quentin <151142649+dorito-brazer@users.noreply.github.com>
2023-11-26 17:58:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
 #Main 
+from pychatbot-chabrun-arnoult-d.py import *
 
+if __name__ == '__main__':
+    Fonction = int(input("Saisir le numéro de la fonctionnalité: "))
+    if Fonction == 1 :
+      mots_pas_imp(matrcie)
+    elif Fonction == 2:
+        mots_score_max(matrice)
+    elif Fonction == 3 :
+        mot_max_chirac(files_names)
+    elif Fonction == 4 :
+        repetition(files_names)
+    elif Fonction == 5 :
+        ecologie(files_names)
+    elif Fonction == 6 :
+        fct3()  
+    else:
+        print("Ce numéro de fonction n'existe pas")
+      
